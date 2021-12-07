4108e5dd290dc149b49e84ddb0b2dfa80804356c
huzaifa003 <buntyhuzaifa3@gmail.com>
2021-12-08 00:52:44 +0500

    Added HashTable & Trie Implementation

---
+++ misclleneous/lab_Project/structure.h
@@ -0,0 +1,11 @@
+#include<conio.h>
+#include<stdio.h>
+#include<stdio.h>
+#define SIZE 15
+struct contact
+{
+       float contactNumber;
+    char Name[SIZE], Email[SIZE], Adress[SIZE];
+    char Relationship[SIZE];
+    bool favourite, Blocked;
+} list;

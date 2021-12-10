42eff4059141d30246d59607ced011ad9a1e73d7
huzaifa003 <buntyhuzaifa3@gmail.com>
2021-12-10 19:01:36 +0500

    Added basic Struct

--- misclleneous/lab_Project/structure.h
+++ misclleneous/lab_Project/structure.h
@@ -1,10 +1,11 @@
 #include<conio.h>
 #include<stdio.h>
 #include<stdio.h>
+#include "NodeSinglyLinkedList.h"
 #define SIZE 15
 struct contact
 {
-       float contactNumber;
+    ;
     char Name[SIZE], Email[SIZE], Adress[SIZE];
     char Relationship[SIZE];
     bool favourite, Blocked;
